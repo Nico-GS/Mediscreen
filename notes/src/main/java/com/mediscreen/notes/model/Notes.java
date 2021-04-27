b290294cff41242b917e90a9d8769f77ca7c0a4c
Nico-GS <ng73800@outlook.fr>
2021-04-27 22:16:02 +0200

    Sprint2 - Notes NoSQL

---
+++ notes/src/main/java/com/mediscreen/notes/model/Notes.java
@@ -0,0 +1,18 @@
+package com.mediscreen.notes.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Document(collection = "notes")
+public class Notes {
+
+
+
+}
---
+++ notes/src/test/java/com/mediscreen/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

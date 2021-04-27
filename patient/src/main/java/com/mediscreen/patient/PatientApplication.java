92be74cbe88fc38072b7887b9ddba28e7cc07e84
Nico-GS <ng73800@outlook.fr>
2021-04-27 17:30:05 +0200

    Feature Ajout Prise de RDV

--- patient/src/main/java/com/mediscreen/patient/PatientApplication.java
+++ patient/src/main/java/com/mediscreen/patient/PatientApplication.java
@@ -3,7 +3,10 @@ package com.mediscreen.patient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PatientApplication {

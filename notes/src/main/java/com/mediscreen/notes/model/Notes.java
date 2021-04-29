f5cd69b1d8f64b38416223934130e4defd38871d
Nico-GS <ng73800@outlook.fr>
2021-04-29 16:13:08 +0200

    Sprint2 - Notes NoSQL Ajout Exception Ajout Controller Ajout service

--- notes/src/main/java/com/mediscreen/notes/model/Notes.java
+++ notes/src/main/java/com/mediscreen/notes/model/Notes.java
@@ -10,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.annotation.Generated;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @AllArgsConstructor
@@ -25,7 +26,16 @@ public class Notes {
     @Field(value = "patientId")
     private int patientId;
 
+    @Field(value = "lastName")
+    @NotBlank(message = "LastName can't be blank")
+    private String lastName;
+
+    @Field(value = "firstName")
+    @NotBlank(message = "FirstName can't be blank")
+    private String firstName;
+
     @Field(value = "note")
+    @NotBlank(message = "Note can't be blank")
     private String note;
 
     @Field(value = "dateNote")

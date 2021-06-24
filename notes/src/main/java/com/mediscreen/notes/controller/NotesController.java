fd0cce8d7c8d5d66a8f00c7d2a16fdcbe5566db9
Nico-GS <ng73800@outlook.fr>
2021-06-24 19:04:50 +0200

    Final Project

--- notes/src/main/java/com/mediscreen/notes/controller/NotesController.java
+++ notes/src/main/java/com/mediscreen/notes/controller/NotesController.java
@@ -4,17 +4,14 @@ import com.mediscreen.notes.exception.ResourceNotFoundException;
 import com.mediscreen.notes.model.Notes;
 import com.mediscreen.notes.repository.NotesRepository;
 import com.mediscreen.notes.service.NotesServices;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
